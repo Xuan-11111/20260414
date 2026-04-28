e5264d94c3e9560e4291c688fad4736ec24d1592
user <a0905343708@gmail.com>
2026-04-28 11:20:37 +0800

    for if

--- JS.js
+++ JS.js
@@ -7,3 +7,10 @@ console.log(
 console.log(
     "西元:"+(bdYear+1910)
 )
+
+const arr =[1, 5, 2, 8, 3]
+let max =arr[0]
+for (let i = 1; i < arr.length; i++)if(arr[i]>max) {
+    max = arr[i];
+}
+console.log(max);
